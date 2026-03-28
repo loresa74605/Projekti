26351ae07a70eac8a8c11ba3cee64f0b5aff3e75
loresademiri <loresademiri2@gmail.com>
2026-03-28 18:50:21 +0100

    Little details changed

--- addRecipe.js
+++ addRecipe.js
@@ -1,13 +1,3 @@
-var i=0;
-var imgArray = [
-    "cupcakes.jpg",
-    "bluess.jpg",
-    "papaya.jpg"
-];
-function ndrroImg(){
-    document.getElementById('slideshow').src=imgArray[i];
-    i=(i+1)%imgArray.length
-}
 function toggleSearch(){
 
 let searchBar = document.getElementById("searchBar");
--- recipes.js
+++ recipes.js
@@ -1,3 +1,15 @@
+function toggleSearch(){
+
+let searchBar = document.getElementById("searchBar");
+
+if(searchBar.style.display === "none" || searchBar.style.display === ""){
+searchBar.style.display = "flex";
+} else{
+searchBar.style.display = "none";
+}
+
+}
+
 function searchRecipes(){
     let input = document.getElementById("searchInput").value.toLowerCase();
     let cards = document.querySelectorAll(".card");
@@ -6,3 +18,37 @@ function searchRecipes(){
         card.style.display = text.includes(input) ? "block" : "none";
     });
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const isAdmin = true;
+    if(!isAdmin){
+        document.getElementById("addRecipeBtn").style.display = "none";
+    }
+    window.toggleFavorite = function(btn){
+        if(btn.textContent === "🤍"){
+            btn.textContent = "❤️"; 
+            btn.classList.add("favorited");
+        } else {
+            btn.textContent = "🤍"; 
+            btn.classList.remove("favorited");
+        }
+    }
+
+    
+    window.showAddRecipeForm = function(){
+        console.log("Admin: Open Add Recipe Form");
+    }
+
+});
+
+function openFavorites() {
+    console.log("Open Favorites page");
+}
+
+function openAccount() {
+    console.log("Open Account page");
+}
+
+
